d1ee37a0b2a6f84d4f4c80abf814b1e3174e6d08
zacmontague <zacander.mon@gmail.com>
2017-09-21 11:48:04 -0700

    Fixed shell scripts for production steps.

--- Ana_EventGeneration/makeTopSample.sh
+++ Ana_EventGeneration/makeTopSample.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 source compile_pythia.sh
-./MakeNTupleFromPythia.exe tt pythia_tt_test$1.root $1
+./MakeNTupleFromPythia.exe tt pythia_tt_$1.root $1
 source compile_ntupler.sh
-./NTupler.exe tt pythia_tt_test$1.root ntuple_tt_test$1.root
+./NTupler.exe tt pythia_tt_$1.root ntuple_tt_$1.root
--- Ana_EventGeneration/makeWSample.sh
+++ Ana_EventGeneration/makeWSample.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 source compile_pythia.sh
-./MakeNTupleFromPythia.exe ww pythia_tt_test$1.root $1
+./MakeNTupleFromPythia.exe ww pythia_ww_$1.root $1
 source compile_ntupler.sh
-./NTupler.exe ww pythia_tt_test$1.root ntuple_tt_test$1.root
+./NTupler.exe ww pythia_ww_$1.root ntuple_ww_$1.root
