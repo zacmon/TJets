e376cbff8c041434b68ab22ba1c7dffaf27b5229
zacmontague <zacander.mon@gmail.com>
2018-01-27 22:29:57 -0800

    Fixed NTupler

--- Ana_EventGeneration/NTupler.cc
+++ Ana_EventGeneration/NTupler.cc
@@ -336,9 +336,7 @@ int main (int argc, char* argv[]) {
 
 	    //  Run telescoping subjet algorithm with trimmed truth jet.
 	    tSub T1SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(1, minR, maxR, numRadii, 0.0);
-            std::cout << "T3Subjet without mass" << std::endl;
-            tSub T2SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(3, minR, maxR, numRadii, 0.0);
-            std::cout << "T3Subjet with mass" << std::endl;
+            tSub T2SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(2, minR, maxR, numRadii, 0.0);
             tSub T3SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(3, minR, maxR, numRadii, 80.4);
             tSub T4SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(4, minR, maxR, numRadii, 0.0);
             tSub T5SubOutputTrim = telescopeTruthGroomedJet->tNSubjet(5, minR, maxR, numRadii, 0.0);
@@ -423,7 +421,7 @@ int main (int argc, char* argv[]) {
 	    //  calorimeter jet.
 	    tSub T1CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(1, minR, maxR, numRadii, 0.0);
 	    tSub T2CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(2, minR, maxR, numRadii, 0.0);
-	    tSub T3CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(3, minR, maxR, numRadii, 0.0);
+	    tSub T3CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(3, minR, maxR, numRadii, 80.4);
 	    tSub T4CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(4, minR, maxR, numRadii, 0.0);
 	    tSub T5CaloJetTrim = telescopeCaloGroomedJet->tNSubjet(5, minR, maxR, numRadii, 0.0);
 
