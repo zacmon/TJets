088627655ad868c0e434f47bad7eccfd0010cf71
youyuan <youyuan.wu@cern.ch>
2019-03-21 20:52:02 -0700

    configure path variables in make file

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# define variables for library locations
+
+# please set path variables to be the installed directory
+# like this: then source this file
+export PYTHIAPATH=/home/wuy45/Temp/pythia8240
+export FASTJETPATH=/home/wuy45/Temp/fastjet-install
